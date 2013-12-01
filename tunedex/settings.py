d650f17f495a3c5b103e3d52ab1754774b9ad2ab
Scot Hacker <shacker@birdhouse.org>
2013-12-01 01:42:41 -0800

    Add static_apps; first implementation of Foundation

--- tunedex/settings.py
+++ tunedex/settings.py
@@ -1,10 +1,8 @@
 import os
 
-
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 ADMINS = (
     ('Scot Hacker', 'shacker@birdhouse.org'),
@@ -63,22 +61,19 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH,'static_apps')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = ( os.path.join(PROJECT_PATH,'static/'),)
 
 # List of finder classes that know how to find static files in
 # various locations.
