344173e10ea16842f0941cf56d3a278ab8dd7c35
Scot Hacker <shacker@birdhouse.org>
2013-12-20 08:29:29 -0800

    Linting

--- tunedex/apps/profiles/views.py
+++ tunedex/apps/profiles/views.py
@@ -1,21 +1,16 @@
-from django.http import HttpResponse
 from profiles.models import Profile
-from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
-                
+
 def profile_display(request, username=None):
     """
     A user's profile display
     """
 
-    profile = get_object_or_404(Profile, user__username__iexact=username) 
-    
-    # items = Item.objects.filter(list=list,completed=False).order_by('priority','-created_date')
-    
+    profile = get_object_or_404(Profile, user__username__iexact=username)
+
     return render(
-    	request,
-    	"profiles/display.html",
-    	locals()
-    )    
-    
+        request,
+        "profiles/display.html",
+        locals()
+    )
