9444ae3ccf948bf74bbac7713050a7b6befb616a
Scot Hacker <shacker@birdhouse.org>
2013-12-21 21:38:50 -0800

    Added config.rb to simplify sass compilation and imports

---
+++ tunedex/config.rb
@@ -0,0 +1,10 @@
+# Path to Zurb Foundation
+add_import_path "static/bower_components/foundation/scss"
+
+# Set this to the root of your project when deployed.
+# django compressor will copy static_common media over to static in deployment
+http_path = "/"
+css_dir = "static/styles/css"
+sass_dir = "static/styles/sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
