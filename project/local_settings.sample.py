3a28e37d51bf1b0fca8aff7589ef5ec7beb070fc
Scot Hacker <shacker@birdhouse.org>
2014-05-28 23:29:39 -0700

    Better instructions for local compressor testing

--- project/local_settings.sample.py
+++ project/local_settings.sample.py
@@ -15,11 +15,6 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# Override compressor settings
-COMPRESS_ENABLED = False
-COMPRESS_OFFLINE = False
-
-
 # django-allauth stores these in the db - optionally copy here for reference
 SOCIAL_AUTH_TWITTER_KEY = 'foo'
 SOCIAL_AUTH_TWITTER_SECRET = 'bar'
@@ -32,3 +27,12 @@ SOCIAL_AUTH_TWITTER_SECRET = 'bar'
 
 # localhost
 SITE_ID = 2
+
+# To test django-compressor's asset pipeline locally:
+# 1) `./manage.py collecstatic`
+# 2) Set both options below to True and run `./manage.py compress`:
+# 3) After compressing, set COMPRESS_OFFLINE back to False.
+# 4) View source to verify that js and css paths have been altered
+# 5) Comment both out when done for general local dev
+# COMPRESS_ENABLED = True
+# COMPRESS_OFFLINE = True
