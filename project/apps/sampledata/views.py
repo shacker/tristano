9e4f31e694e03009e7660a21ee8e6d7dcd1b6c37
Scot Hacker <shacker@birdhouse.org>
2014-08-26 15:07:40 -0700

    Generic CBV examples for Book list and detail views

--- project/apps/sampledata/views.py
+++ project/apps/sampledata/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+
+from sampledata.models import Book
+
 
 
 class BooksListView(TemplateView):
@@ -7,3 +11,19 @@ class BooksListView(TemplateView):
     """
 
     template_name = "books/list.html"
+
+
+class BooksStaticListView(ListView):
+    '''
+    Example of class-based list view for static display
+    '''
+    model = Book
+    template_name = "books/list_static.html"
+
+
+class BooksStaticDetailView(DetailView):
+    '''
+    Example of class-based detail view for static display
+    '''
+    model = Book
+    template_name = "books/detail_static.html"
--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from rest_framework import viewsets, routers
 
 from profiles.views import ProfileDetailView, ProfileUpdateView, UserViewSet
-from sampledata.views import BooksListView
+from sampledata.views import BooksListView, BooksStaticListView, BooksStaticDetailView
 from sampledata.views_api import BookViewSet
 from sampledata.models import Book
 
@@ -33,7 +33,9 @@ urlpatterns = patterns('',
     url(r'^p/(?P<username>\w+)/$', ProfileDetailView.as_view(),  name='profile_detail'),
 
     # My Books
-    url(r'^books/?$', BooksListView.as_view(),  name='books_list'),
+    url(r'^books/staticlist/?$', BooksStaticListView.as_view(),  name='books_list_static'),  # Static list view
+    url(r'^books/staticdetail/(?P<pk>[\d]+)?$', BooksStaticDetailView.as_view(),  name='books_detail_static'),  # Static detail view
+    url(r'^books/?$', BooksListView.as_view(),  name='books_list'),  # Angular view
 
     # API
     url(r'^api/', include(router.urls)),
